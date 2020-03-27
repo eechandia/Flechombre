05a73bb800f8ad8398fd92b9e5350dadc9aff9a5
Pedro Molteni <pedromolteni72@gmail.com>
2020-03-27 15:11:36 -0300

    subi la window y el gamecontainer

---
+++ src/com/pedro/engine/GameContainer.java
@@ -0,0 +1,129 @@
+package com.pedro.engine;
+
+public class GameContainer implements Runnable{
+	
+	private Thread thread;
+	
+	private Window window;
+	
+	private boolean running = false;
+	private final double UPDATE_CAP = 1.0/60.0;
+	private int width = 320, height = 240;
+	private float scale = 3.5f;
+	private String title = "PruebaEngine v1.0";
+	
+	public GameContainer() {
+		
+	}
+	
+	public void start() {
+		window = new Window(this);
+		
+		thread = new Thread(this);
+		thread.run(); // .run para ser la main thread// .start para q sea secundaria (side thread)
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public void run() {
+		running = true;
+		
+		boolean render = false;
+		double first_time = 0;
+		double last_time = System.nanoTime()/1000000000.0;
+		double passed_time = 0;
+		double unprocessed_time = 0;
+		
+		double frame_time = 0;
+		int frames = 0;
+		int fps = 0;
+		
+		while(running) {
+			
+			render = false;
+			
+			first_time = System.nanoTime()/1000000000.0;
+			passed_time = first_time - last_time;
+			last_time = first_time; 
+			
+			unprocessed_time += passed_time;
+			frame_time += passed_time;
+			
+			while(unprocessed_time >= UPDATE_CAP) {
+				
+				unprocessed_time -= UPDATE_CAP;
+				render = true;
+				
+				//To do: Update game
+				if(frame_time >= 1.0) {
+					frame_time = 0;
+					fps = frames;
+					frames = 0;
+					
+					System.out.println("FPS: "+fps); 
+				}
+			}
+			
+			if(render) {
+				//To do: Render game
+				window.update();
+				frames++;
+			}
+			else {
+				try {
+					Thread.sleep(1);
+				}
+				catch(InterruptedException e){
+					 e.printStackTrace();
+				}
+				
+			}
+		}
+		
+		dispose();
+		
+	}
+	
+	private void dispose() {
+		
+	}
+	
+	public static void main(String args[]){
+		GameContainer gc = new GameContainer();
+		gc.start();
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public float getScale() {
+		return scale;
+	}
+
+	public void setScale(float scale) {
+		this.scale = scale;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
---
+++ src/com/pedro/engine/Window.java
@@ -0,0 +1,52 @@
+package com.pedro.engine;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+public class Window {
+	
+	private JFrame frame;
+	private BufferedImage image;
+	private Canvas canvas;
+	private BufferStrategy bs;
+	private Graphics graphics;
+	
+	public Window(GameContainer gc) { //gc = width, height, scale, input...
+		 
+		 image = new BufferedImage(gc.getWidth(), gc.getHeight(), BufferedImage.TYPE_INT_RGB);
+		 canvas = new Canvas();
+		 Dimension s = new Dimension((int)(gc.getWidth()*gc.getScale()), (int)(gc.getHeight()*gc.getScale())); 
+		 canvas.setPreferredSize(s);
+		 canvas.setMaximumSize(s);
+		 canvas.setMinimumSize(s);
+		 
+		 frame = new JFrame(gc.getTitle());
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 frame.setLayout(new BorderLayout());
+		 frame.add(canvas, BorderLayout.CENTER);
+		 frame.pack();
+		 frame.setLocationRelativeTo(null);
+		 frame.setResizable(false);
+		 frame.setVisible(true);
+		 
+		 canvas.createBufferStrategy(2);
+		 bs = canvas.getBufferStrategy();
+		 graphics = bs.getDrawGraphics();
+				  
+	}
+	
+	public void update(){
+	
+		graphics.drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight(), null);
+		bs.show();
+	
+	}
+	
+	
+}
